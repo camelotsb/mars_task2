26a941fda9668c33283b7d544ef227518107ed1d
camelotsb <dynamist.sb@gmail.com>
2022-07-24 11:32:26 +0530

    Update checkcollision.cpp

--- src/samplepkg/checkcollision.cpp
+++ src/samplepkg/checkcollision.cpp
@@ -3,32 +3,31 @@
 #include <stdlib.h>
 #include <std_msgs/Bool.h>
 #include <iomanip>
+#include <iostream>
 
-void sensorDataRecieved(const geometry_msgs::Twist& msg) {
-    float k=msg.linear.x;
-    ros::NodeHandle r;
-    ros::Publisher e_pub= r.advertise<std_msgs::Bool>("epath",1000);
-    std_msgs::Bool de;
-    
-    if (k>5)
-    {
-        ROS_INFO_STREAM("The rover is safe to proceed: 1");
-        de.data=true;
-        e_pub.publish(de);
+std_msgs::Bool vp;
+
+void collisiond(const geometry_msgs::Twist& d){
+    if (d.linear.x>5){
+        vp.data=1;
     }
     else{
-        ROS_INFO_STREAM("Danger !! the rover will collide in "<<msg.linear.x<<"centimeters.");
-        
-        de.data=false;
-        e_pub.publish(de);
+        vp.data=0;
     }
 }
 
 int main(int argc,char **argv){
-
+    vp.data=1;
     ros::init(argc,argv,"subscribe_to_sensor");
     ros::NodeHandle nh;
-
-    ros::Subscriber sub=nh.subscribe("randomsensor",1000,&sensorDataRecieved);
-    ros::spin();
+    ros::Publisher pub=nh.advertise<std_msgs::Bool>("epath",1000);
+    ros::Subscriber sub=nh.subscribe<geometry_msgs::Twist>("randomsensor",1000,&collisiond);
+    while(ros::ok())
+    {
+        pub.publish(vp);
+        ros::spinOnce();
+    }
+    return 0;   
 }
+
+
